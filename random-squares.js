c5cf999924edd16bac55386ead844fbeba788ef3
Majd <majd.jrrar1@gmail.com>
2024-06-24 19:39:30 -0400

    added cut method that doesn't work
    replaced random numbers with refrence to variables for readablity

--- random-squares.js
+++ random-squares.js
@@ -1,27 +1,30 @@
-// welcome to blot!
-
-// check out this guide to learn how to program in blot
-// https://blot.hackclub.com/editor?guide=start
-
 const frameWidth = 125
 const frameHeight = 125
+const frame = [
+  [0, 0],
+  [frameWidth, 0],
+  [frameWidth, frameHeight],
+  [0, frameHeight],
+  [0, 0]
+]
 
 setDocDimensions(frameWidth, frameHeight)
 
 function createSquare(width, height) {
-  let square = 
-    [
-      [0, 0],
-      [width, 0],
-      [width, height],
-      [0, height],
-      [0, 0]
-    ]
-  
-  bt.rotate([square], bt.randInRange(0,360))
-  bt.translate([square], [bt.randInRange(0, 125), bt.randInRange(0, 125)])
+  let square = [
+    [0, 0],
+    [width, 0],
+    [width, height],
+    [0, height],
+    [0, 0]
+  ]
+
+
+  bt.rotate([square], bt.randInRange(0, 360))
+  bt.translate([square], [bt.randInRange(0, frameWidth), bt.randInRange(0, frameHeight)])
+  bt.cut([square], [frame])
   return square
 }
 
-
+drawLines([frame])
 drawLines([createSquare(bt.randInRange(10, 50), bt.randInRange(10, 50))])
