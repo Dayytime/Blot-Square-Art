d8c08bbc0976d7d9ccdeef69572d7402b31ff4d2
Majd <majd.jrrar1@gmail.com>
2024-06-25 19:19:02 -0400

    added more shapes

--- random-squares.js
+++ random-squares.js
@@ -10,24 +10,59 @@ const frame = [[
 
 setDocDimensions(frameWidth, frameHeight)
 
-function createSquare(width, height) {
-  let valid = true
-  let square = [[
-    [0, 0],
-    [width, 0],
-    [width, height],
-    [0, height],
-    [0, 0]
-  ]]
-
-  
-  bt.rotate(square, bt.randInRange(0, 360))
-  bt.translate(square, [bt.randInRange(0, frameWidth), bt.randInRange(0, frameHeight)])
-  bt.cut(square, frame)
-  return square
+function createRec(width, height){
+  const t = new bt.Turtle()
+  t.forward(width)
+  t.left(90)
+  t.forward(height)
+  t.left(90)
+  t.forward(width)
+  t.left(90)
+  t.forward(height)
+  return t.path
 }
 
-drawLines([frame])
-for(let i = 0; i <= 25; i++){
-  drawLines(createSquare(bt.randInRange(10, 50), bt.randInRange(10, 50)))
+function createCircle(radius){
+  const t = new bt.Turtle()
+  t.arc(360, radius)
+  console.log("circle")
+  return t.path
+}
+
+function createTriangle(size){
+  const t = new bt.Turtle()
+  t.forward(size)
+  t.left(120)
+  t.forward(size)
+  t.left(120)
+  t.forward(size)
+  console.log("triangle")
+  return t.path
+}
+
+function generateShapes() {
+  let randomChoice = bt.randIntInRange(1,3)
+  let shape = []
+  console.log(randomChoice)
+
+  if (randomChoice == 1){
+    shape = createRec(bt.randInRange(10, 50), bt.randInRange(10, 50))
+  }
+  else if (randomChoice == 2){
+    shape = createCircle(bt.randInRange(10, 50))
+  }
+  else {
+    shape = createTriangle(bt.randInRange(10, 50))
+  }
+
+  
+  bt.rotate(shape, bt.randInRange(0, 360))
+  bt.translate(shape, [bt.randInRange(0, frameWidth), bt.randInRange(0, frameHeight)])
+  bt.cut(shape, frame)
+  return shape
+}
+
+drawLines(frame)
+for(let i = 0; i <= 20; i++){
+  drawLines(generateShapes())
 }
